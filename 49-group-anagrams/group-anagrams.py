388c1bf474952ff31bda88454d821cf3279bb541
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-05 21:22:56 -0400

    Time: 12 ms (98.51%) | Memory: 16.9 MB (37.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         sort_dict = {}
         for wrd in strs:
-            sorted_wrd = "".join(sorted(wrd))
+            sorted_wrd = tuple(sorted(wrd))
             if sorted_wrd in sort_dict:
                 sort_dict[sorted_wrd].append(wrd)
             else:
