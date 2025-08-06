59bcc7ad7e4df18fc9656b8a0656f4a057d46e7a
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-05 21:12:20 -0400

    Time: 23 ms (57.04%) | Memory: 15.9 MB (98.91%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        sort_dict = {}
+        for wrd in strs:
+            sorted_wrd = "".join(sorted(list(wrd)))
+            if sorted_wrd in sort_dict:
+                sort_dict[sorted_wrd].append(wrd)
+            else:
+                sort_dict[sorted_wrd] = [wrd]
+        ret_list = []
+        for key in sort_dict:
+            ret_list.append(sort_dict[key])
+        return ret_list
+        
