4f2603caae32a0d33f1df72df661d44324eeebc5
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-05 21:18:15 -0400

    Time: 27 ms (41.44%) | Memory: 16.1 MB (64.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,12 +4,9 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        sort_dict = {}
+        sort_dict = defaultdict(list)
         for wrd in strs:
             sorted_wrd = "".join(sorted(list(wrd)))
-            if sorted_wrd in sort_dict:
-                sort_dict[sorted_wrd].append(wrd)
-            else:
-                sort_dict[sorted_wrd] = [wrd]
+            sort_dict[sorted_wrd].append(wrd)
         return list(sort_dict.values())
         
