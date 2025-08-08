329f106bd8b5a6862061fbbe100344e5be82e6ed
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-08 13:50:52 -0400

    Time: 38 ms (39.53%) | Memory: 20.2 MB (67.19%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        prod_dict = {}
+        #prod_dict = {}
+        prod_lst = []
         prod = 1
         for index, num in enumerate(nums):
-            prod_dict[index] = prod
+            #prod_dict[index] = prod
+            prod_lst.append(prod)
             prod *= num
         prod = 1
         for index in range(len(nums)-1, -1, -1):
-            prod_dict[index] *= prod
+            prod_lst[index] *= prod
             prod *= nums[index]
         
-        return prod_dict.values()
+        return prod_lst
         
 
         
