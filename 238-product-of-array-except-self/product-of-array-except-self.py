cfb424cc9bc7b3e9887a60a9f9fbb90f45616cfe
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-08 13:47:30 -0400

    Time: 38 ms (39.53%) | Memory: 25.1 MB (6.15%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,21 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        prev_prod = {}
+        prod_dict = {}
         prod = 1
         for index, num in enumerate(nums):
-            prev_prod[index] = prod
+            prod_dict[index] = prod
             prod *= num
-        after_prod = {}
         prod = 1
         for index in range(len(nums)-1, -1, -1):
-            after_prod[index] = prod
+            prod_dict[index] *= prod
             prod *= nums[index]
         
-        answ = []
-        for index in range(len(nums)):
-            answ.append(prev_prod[index] * after_prod[index])
-        return answ
+        return prod_dict.values()
         
 
         
