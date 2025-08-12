f8c6c4585bcf36a3049a6de12a4b4c65aa552ffb
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-12 18:59:54 -0400

    Time: 112 ms (63.43%) | Memory: 20.7 MB (86.53%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -8,12 +8,9 @@ class Solution(object):
         start, end = 0, len(height) - 1
         while start < end:
             area = max((min(height[start], height[end]) * (end - start)), area)
-            if height[start] > height[end]:
-                end -= 1
-            elif height[start] < height[end]:
+            if height[start] < height[end]:
                 start += 1
             else:
-                start += 1
                 end -= 1
         return area
 
