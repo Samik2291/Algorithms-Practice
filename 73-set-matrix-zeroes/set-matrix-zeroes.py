2c4bf11f9c7a368acec43609f87ff0100f2ba3f6
Samik2291 <108240948+Samik2291@users.noreply.github.com>
2025-08-15 15:00:42 -0400

    Time: 26 ms (7.31%) | Memory: 13.2 MB (41.10%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,21 +5,18 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         rows = []
+        zero_row = [0] * len(matrix[0])
         cols = []
         for row_indx in range(len(matrix)):
             for col_indx in range(len(matrix[row_indx])):
                 if matrix[row_indx][col_indx] == 0:
                     rows.append(row_indx)
                     cols.append(col_indx)
-        '''
         for row_indx in range(len(matrix)):
             if row_indx in rows:
-                for col_indx in range(len(matrix[row])):
-                    matrix[row_indx][col_indx] = 0
-        '''
-        for row_indx in range(len(matrix)):
+                matrix[row_indx] = zero_row
             for col_indx in range(len(matrix[row_indx])):
-                if col_indx in cols or row_indx in rows:
+                if col_indx in cols:
                     matrix[row_indx][col_indx] = 0
 
 
