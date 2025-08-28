8735081097c7b9811a334cd262e8a89027d8c6c6
Samik Mukherjee <108240948+Samik2291@users.noreply.github.com>
2025-08-27 20:15:04 -0400

    Time: 2001 ms (5.02%) | Memory: 19.5 MB (59.94%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        lst = []
+        while head:
+            if head in lst:
+                return True
+            lst.append(head)
+            head = head.next
+        return False
+
+        
