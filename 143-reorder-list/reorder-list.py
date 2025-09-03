3d0a8d2cb4edecb63ca3b9a612b39cdab8219292
Samik Mukherjee <108240948+Samik2291@users.noreply.github.com>
2025-09-03 15:07:53 -0400

    Time: 12 ms (14.65%) | Memory: 30.1 MB (63.14%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -9,29 +9,28 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: None Do not return anything, modify head in-place instead.
         """
-        #step 1: find middle
-        if not head: return []
-        slow, fast = head, head
-        while fast.next and fast.next.next:
+        slow = fast = head
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
-        #step 2: reverse second half
-        prev, curr = None, slow.next
-        while curr:
-            nextt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nextt    
-        slow.next = None
+        reverse, mid = None, slow
+        while mid:
+            reverse, reverse.next, mid = mid, reverse, mid.next
         
-        #step 3: merge lists
-        head1, head2 = head, prev
-        while head2:
-            nextt = head1.next
-            head1.next = head2
-            head1 = head2
-            head2 = nextt
+        first = head
+        while reverse.next:
+            first_next = first.next
+            reverse_next = reverse.next
+            reverse.next = first_next
+            first.next = reverse
+            first = first.next.next
+            reverse = reverse_next
+            
+
+            
+
+        
 
             
 
